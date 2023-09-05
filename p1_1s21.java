c3f4b96797985e3097e3389c2cb0348543f4edc5
drmonsrios20 <144178868+drmonsrios20@users.noreply.github.com>
2023-09-05 16:02:12 -0600

    Add files via upload
    practicas 1y2  martes 5 de septiembre de 2023

---
+++ p1_1s21.java
@@ -0,0 +1,3 @@
+class p1_1s21{
+public static void main (string ){
+  system.out.println("este es nuestro pograma")}
---
+++ p2_1s21.java
@@ -0,0 +1,11 @@
+class p2_1s21{
+   public static void main (string[]args){
+   	System.out.println("datos personales");
+   	Nombre.out.println("Estefanny Montserrat Dominguez Rios")
+   	Edad.out.println("19")
+   	Correo.out.println("riosestefanny83@gmail.com")
+   	Aficion.out.println("estudiante")
+   	Color favorito.out.println("azul")
+
+   }
+}   
