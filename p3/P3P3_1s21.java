2d428c2e000c16c20090bf18aed2ef58c4a9fe4f
drmonsrios20 <144178868+drmonsrios20@users.noreply.github.com>
2023-11-21 14:57:27 -0600

    Add files via upload

---
+++ p3/P3P3_1s21.java
@@ -0,0 +1,6 @@
+class P3p3_1S21 {
+	public static void main(String[] args) {
+		double numero = 2;
+		double elevado = Math.pow(numero, 32);
+		System.out.println("El 2 elevado a la potencia 32 es " + elevado);
+	}
